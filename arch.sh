08edbf8cb759220213a66ff157dd2b4827de1dbe
charlie-s-labs <70104595+charlie-s-lab@users.noreply.github.com>
2023-12-27 19:38:33 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,10 +3,10 @@
 sudo pacman -Syyu --noconfirm
 
 #install some needed apps
-sudo pacman -S curl --noconfirm
+sudo pacman -S curl --noconfirm --needed
 
 #install some wanted apps
-sudo pacman -S fish lsd neovim zsh ranger bottom fastfetch --noconfirm
+sudo pacman -S lsd neovim zsh ranger bottom fastfetch zsh-autosuggestion zsh-completions zsh-syntax-highlighting --noconfirm
 
 #set up zsh
 cd $HOME
