a1cf5c6b4465a17d488a12e2b2d3a21f8945ab29
charlie-s-labs <70104595+charlie-s-lab@users.noreply.github.com>
2024-01-03 12:39:25 +0100

    Update arch.sh
    added ranger

--- arch.sh
+++ arch.sh
@@ -6,7 +6,7 @@ sudo pacman -Syyu --noconfirm
 sudo pacman -S curl --noconfirm --needed
 
 #install some wanted apps
-sudo pacman -S lsd neovim zsh ranger bottom fastfetch zsh-autosuggestion zsh-completions zsh-syntax-highlighting --noconfirm
+sudo pacman -S lsd neovim ranger zsh ranger bottom fastfetch zsh-autosuggestion zsh-completions zsh-syntax-highlighting --noconfirm
 
 #set up zsh
 cd $HOME
